0fac39f5faa898965aaa5d837e8e106b3ae6b506
conongchamchiofficial <conongchamchi.official@gmail.com>
2025-10-08 19:09:43 +0800

    Update and rename test_align_layer to test_align_layer.py

--- test_align_layer.py
+++ test_align_layer.py
@@ -1,13 +1,34 @@
-def get_cost(a, b, args, cost="euclidean"):
-    if cost == "euclidean":
+import random
+import sys
+from basic_config import PATH_TO_CIFAR
+sys.path.append(PATH_TO_CIFAR)
+import time
+
+import numpy as np
+import ot
+import routines
+import torch
+import torch.nn.functional as F
+import train as cifar_train
+import utils
+from data import get_dataloader
+from ground_metric import GroundMetric
+from test_layer_similarity import cca, cka, gram_linear
+from log import logger
+from torch.autograd import Variable
+from wasserstein_ensemble import get_network_from_param_list
+
+
+def get_cost(a, b, args):
+    if args.similarity_type == "euclidean":
         return (a - b) ** 2
-    elif cost == "cca":
+    elif args.similarity_type == "cca":
         return 1 - cca(a, b)
-    elif cost == "cka":
+    elif args.similarity_type == "cka":
         return 1 - cka(gram_linear(a), gram_linear(b))
-    elif cost == "wd":
+    elif args.similarity_type == "wd":
         return get_wasserstein_distance(a, b, args)
-    elif cost == "cosine":
+    elif args.similarity_type == "cosine":
         return F.normalize(a, dim=0) @ F.normalize(b, dim=0).T
     else:
         raise NotImplementedError
