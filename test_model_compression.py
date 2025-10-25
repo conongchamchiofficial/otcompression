494c9ccaf215a60c0f2a3919963f1166719b12c3
conongchamchiofficial <conongchamchi.official@gmail.com>
2025-10-25 16:07:06 +0800

    Update test_model_compression.py

--- test_model_compression.py
+++ test_model_compression.py
@@ -285,7 +285,7 @@ def compress_model(args, networks, accuracies, num_layers, model_names=None):
         print("Model {} has accuracy of {} with {} layers and parameters".format(i, accuracies[i],num_layers[i]))
         print(networks)
 
-    dissimilarity_matrix = get_dissimilarity_matrix(args, networks, num_layers, model_names)
+    dissimilarity_matrix = get_dissimilarity_matrix1(args, networks, num_layers, model_names)
   
     return args, networks, accuracies, num_layers, model_names
 
