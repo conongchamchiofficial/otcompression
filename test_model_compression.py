b7256933f7300e131948c75b3f28759f7dc8fc34
conongchamchiofficial <conongchamchi.official@gmail.com>
2025-11-11 15:41:56 +0800

    Update test_model_compression.py

--- test_model_compression.py
+++ test_model_compression.py
@@ -430,7 +430,10 @@ def merge_layers(args, networks, num_layers, model_names, acts, I, method):
             if layer != grp[-1]:
                 print(f"Merge layer {layer} with {grp[idx_layer + 1]}")
                 print("Approximate ReLU at hidden layer {} with activation of shape {}".format(layer + 1, acts[layer].shape))
-                act_vec = approximate_relu(acts[layer].T, layer_weight.shape[1], args, method)
+                if args.layer_metric == "wd":
+                    act_vec = approximate_relu(acts[layer], layer_weight.shape[1], args, method)
+                else: 
+                    act_vec = approximate_relu(acts[layer].T, layer_weight.shape[1], args, method)
                 print("act_vec.shape == layer_weight.shape", act_vec.shape, layer_weight.shape)
                 assert act_vec.shape == layer_weight.shape
                 if not isinstance(act_vec, torch.Tensor):
